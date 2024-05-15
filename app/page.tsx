633a697fdb2f7b58aab41a1e7c722376047f67f2
Mariya B <supermariyabros@gmail.com>
2024-05-15 15:56:31 +0300

    ✨ Fix merge conflict

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,11 @@
 import React from 'react';
-<<<<<<< HEAD
 
 import Hero from "@/components/Home/Hero";
-import Cards from "@/components/Home/Cards";
-=======
-import Hero from "@/components/Home/Hero"
-import Card from '@/components/Home/Card';
->>>>>>> parent of f3dc958 (🌈 Small Hero Fix ✨)
 
 export default function Home() {
 	return (
 		<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
 			<Hero />
-<<<<<<< HEAD
-			<Cards />
-=======
-			<Card />
->>>>>>> parent of f3dc958 (🌈 Small Hero Fix ✨)
 		</section>
 	);
 }
