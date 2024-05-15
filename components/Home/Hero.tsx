959378565b674790889d4d267e2bc5e2c8a4ba1a
Mariya B <supermariyabros@gmail.com>
2024-05-15 15:52:28 +0300

    Revert "🌈 Small Hero Fix ✨"
    This reverts commit f3dc958b8e12df610feed090e6c585b157a63b4f.

--- components/Home/Hero.tsx
+++ components/Home/Hero.tsx
@@ -4,7 +4,7 @@ import Logo from "@/public/LOGO.png";
 
 const Hero = () => {
   return (
-    <section className="bg-black">
+    <section className="bg-black hero">
       <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
         <div className="mr-auto place-self-center lg:col-span-7">
           <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xltext-white">
